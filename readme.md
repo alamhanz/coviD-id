e0e14d3cb53449d68eccd8b294f2d7c6299b074b
alamhanz <alamsyah.koto@go-jek.com>
2020-03-29 19:21:39 +0700

    analisa awal

---
+++ readme.md
@@ -0,0 +1,38 @@
+# Covid Modelling
+
+Ini merupakan repositori untuk pemodelan penyebaran Covid di Indonesia. Analisa dilakukan dengan data yang terus di update dari tanggal 26 Maret - (_on going analysis_).
+
+
+## Analysis
+
+Analisa yang dilakukan menyesuaikan ketersediaan data yang ada. Data yang tersedia hanya data gabungan semua kasus COVID-19 setiap harinya di Indonesia. Terima kasih [kawalcovid19](https://kawalcovid19.id/) untuk menyediakan datanya. Analisa yang dilakukan akan lebih _Predictive Analysis_, analisa ini dilakukan untuk melihat bagaimana perkembangan COVID-19 di Indonesia kedepannya.
+
+### Update (2020/03/26)
+
+Analisa dilakukan dengan melakukan fit-ing data yang tersedia dengan SIRD model. Model ini membutuhkan parameter yang sesuai sehingga SIRD akan bisa dipakai untuk memprediksi. 
+
+![SIRD](images/Scheme-of-Susceptible-Infectious-Recovered-Death-SIRD-Model-Boxes-represent.png)
+
+Parameter SIRD masih mungkin untuk di estimasi dengan Regresi Linear dengan mengestimasi nilai turunan S,I,R, dan D terhadap waktu. Nilai turunan ini bisa didapat dengan mengurangi nilai dari salah satu `compartment value` tersebut di waktu `t` dan `t-1`
+
+### Update (2020/03/28)
+
+Pendekatan dengan menggunakan Regresi Linear tidak memberikan hasil yang optimal. Untuk itu, Estimasi parameter dilakukan dengan mengoptimasi _cost function_ yang telah di definisikan. Sehingga estimasi parameter bisa dilakukan dengan _Bayes optimization_.
+
+### Update (2020/03/29)
+
+Cukup memakan waktu lama untuk menemukan parameter yang sesuai dengan menggunakan _Bayes optimization_. Metode optimisasi tersebut juga harus masih diawasi dalam menemukan hasilnya (belum menemukan cara automasinya). Untuk itu dicoba menggunakan optimisasi lainnya, yaitu [_PSO Algorithm_](https://medium.com/analytics-vidhya/implementing-particle-swarm-optimization-pso-algorithm-in-python-9efc2eb179a6). Harapannya, optimisasi ini bisa membantu mencari parameter yang optimal untuk berbagai _case of data_ dalam fitting model SIRD ini.
+
+Hasil dari tuning parameternya menunjukan adanya ketimpangan antara Recovery rate dan Death rate (Rr < Dr). Sebagai tambahan, Interaction rate antara `Suc` dan `Inf` mulai terlihat berkurang.. yang artinya banyak penduduk Indonesia yg melakukan Social Distancing.
+Prediction : kasus baru hari ini bisa mencapai hampir 200 .. 
+
+
+## Refensi
+    
+    [Data](https://kawalcovid19.blob.core.windows.net/viz/statistik_harian.html)
+    
+    [Source Script](http://epirecip.es/epicookbook/chapters/kr08/2_1/python_original)
+    
+    [Image 1](https://www.researchgate.net/figure/Scheme-of-Susceptible-Infectious-Recovered-Death-SIRD-Model-Boxes-represent_fig1_41507287)
+    
+    [Image 2](https://www.chegg.com/homework-help/questions-and-answers/codes-problem-code-1-function-siddeterm-simulation-deterministic-sird-model-using-differen-q21316613)
---
+++ src/covid_model.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import scipy.integrate as spi
+import numpy as np
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import datetime as dt
+import pyswarms as ps
+
+## basic sird model
+def sird_model_diff(VALUE_ON_T,t,param_bk,param_rk,param_mk):  
+    '''The main set of equations'''
+    Y=np.zeros((4))
+    dS,dI,dR,dD = VALUE_ON_T  
+    Y[0] = - param_bk * dS * dI
+    Y[1] = (param_bk * dS * dI) - (param_rk * dI) - (param_mk * dI)
+    Y[2] = param_rk * dI
+    Y[3] = param_mk * dI
+    return Y
+
+def sird_model(x,INPUT,T_range):
+    param_b,param_r,param_m=x
+    RES = spi.odeint(sird_model_diff,INPUT,T_range, args=(param_b,param_r,param_m))
+    return RES
+
+def cost_func_sird(x,df,N,with_pred=False):
+    ## Parameter
+    param_b,param_r,param_m=x
+    
+    ## time range
+    T_start = 0.0
+    T_inc=1.0
+    T_end=df.shape[0]
+    
+    ## Initial input
+    I0=2/N
+    S0=1-I0
+    N0=N*1000
+        
+    INPUT = (S0, I0, 0.0, 0.0)
+    col_use=['Sus','Inf','Rec','Dea']
+    col_use_pred=[x+'_pred' for x in col_use]
+    T_range = np.arange(T_start, T_end+T_inc, T_inc)
+    RES = spi.odeint(sird_model_diff,INPUT,T_range, args=(param_b,param_r,param_m))
+    df[col_use_pred]=pd.DataFrame(RES)
+
+    eS=(((df['Sus']-df['Sus_pred'])*df['weight'])**2).sum()*0.15
+    eI=(((df['Inf']-df['Inf_pred'])*df['weight'])**2).sum()*0.35
+    eR=(((df['Rec']-df['Rec_pred'])*df['weight'])**2).sum()*0.3
+    eD=(((df['Dea']-df['Dea_pred'])*df['weight'])**2).sum()*0.2
+
+    if with_pred:
+        return df,(eS+eI+eR+eD)*N0  
+    else:
+        return (eS+eI+eR+eD)*N0
+
+def multi_cost_func_sird(x,df,N):
+    all_res=[]
+    for i in x:
+        res=cost_func_sird(i,df,N)
+        all_res.append(res)
+    return np.array(all_res)
+    
+def pso_sird(df0,population):
+    opt1 = {'c1': 0.15, 'c2': 0.95, 'w':0.25}
+    pos_res=[]
+    best_pos=[]
+    min_func=100
+    for i in range(3):
+        optimizer1 = ps.single.GlobalBestPSO(n_particles=200, dimensions=3, options=opt1,
+                                             bounds=([0.1,0.005,0.005],[0.7,0.3,0.3]))
+        # Perform optimization
+        cost, pos = optimizer1.optimize(multi_cost_func_sird, iters=20, df=df0, N=population)
+        pos_res.append(pos)
+        if cost<min_func:
+            min_func=cost
+            best_pos=pos
+
+    max_bound2=np.array(pos_res).max(axis=0)
+    max_bound2=max_bound2+(max_bound2*0.08)
+    min_bound2=np.array(pos_res).min(axis=0)
+    min_bound2=min_bound2-(min_bound2*0.08)
+    optimizer2 = ps.single.GlobalBestPSO(n_particles=150, dimensions=3, options=opt1,
+                                             bounds=(min_bound2,max_bound2))
+    cost, pos = optimizer2.optimize(multi_cost_func_sird, iters=40, df=df0, N=population)
+    pos_res.append(pos)
+    if cost<min_func:
+        min_func=cost
+        best_pos=pos
+    return pos,min_func
